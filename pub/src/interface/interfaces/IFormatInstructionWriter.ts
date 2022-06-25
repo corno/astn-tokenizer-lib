315a58628d652ac16905078ca0a1a109db670ed1
corno <corno.astn.a@schraverus.com>
2022-06-25 10:28:52 +0200

    removed superfluous

--- pub/src/interface/interfaces/IFormatInstructionWriter.ts
+++
@@ -1,6 +0,0 @@
-import { NonTokenFormatInstruction, TokenFormatInstruction } from "../types/FormatInstruction"
-
-export type IFormatInstructionWriter<EventAnnotation> = {
-    token: (instruction: TokenFormatInstruction, annotation: EventAnnotation) => void
-    nonToken: (instruction: NonTokenFormatInstruction) => void
-}
